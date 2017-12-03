fixed softmax - it seems

crystal_quest/crystal_quest_env.py
@@ -299,14 +299,14 @@ class Wave1Env(gym.Env):
         elif self.num_aliens==2:
             self.alien_locations = np.array([[38,24],[0,24]],dtype=np.float) #2
         self.alien_velocities = [DOWN*2 for _ in range(self.num_aliens)]#[self._random_vel() for _ in range(2)]
-        
+
         #Clumping
         _,clumps = self._random_points(self.num_crystal_clumps+self.num_asteroid_clumps,self.acceptable_points)
         self.crystal_clumps = clumps[:self.num_crystal_clumps]
         self.asteroid_clumps = clumps[:self.num_asteroid_clumps]
 
         self.object_mask = np.ones(len(self.acceptable_points))
-        
+
 
         probs = self._clumping_probs(self.acceptable_points,self.asteroid_clumps,mask=self.object_mask)
         inds,self.asteroid_locations = self._random_points(self.num_asteroids,self.acceptable_points,p=probs)#self._random_points(self.min_obj_loc, self.max_obj_loc,self.num_asteroids) #4
@@ -321,8 +321,6 @@ class Wave1Env(gym.Env):
         # self.acceptable_points = []
          # random_stuff[:self.num_crystals] #(self.min_obj_loc, self.max_obj_loc,self.num_crystals)#np.array([(1,1),(6,4),(8,9),(15,17),(16,21)],dtype=np.int) #3
         
-
-
 
         # self.crystal_locations = self._clump(self.crystal_locations,self.crystal_clumps)
         # self.asteroid_locations = self._clump(self.asteroid_locations,self.asteroid_clumps)

dqn/dqn_graph.py
@@ -36,7 +36,10 @@ class QGraph():
         # q values for current s,a
         self.q_val_t = self.q_func(self.obs_t_float, self.num_actions, scope="q_func", reuse=False)
 
-        self.q_probs = tf.exp(self.q_val_t ) / tf.reduce_sum(tf.exp(self.q_val_t ))
+        #self.q_probs = tf.exp(self.q_val_t ) / tf.reduce_sum(tf.exp(self.q_val_t),axis=1) #ah.. it was reducing it across batch and val
+        self.q_probs = tf.nn.softmax(self.q_val_t,dim=1) # #ah.. it was reducing it across batch and val
+
+        print(np.shape(self.q_probs))
 
         # q value for action taken
         self.q_val_t_selected = tf.reduce_sum(self.q_val_t*tf.one_hot(self.act_t_ph,self.num_actions),axis=1)
@@ -56,6 +59,7 @@ class QGraph():
             self.q_val_t_selected_target = self.rew_t_ph + self.gamma*self.q_val_tp1_softmax_masked
         else:
             self.q_val_t_selected_target = self.rew_t_ph + self.gamma*self.q_val_tp1_best_masked
+
 
         # temporal difference error
         self.td_error = self.q_val_t_selected-tf.stop_gradient(self.q_val_t_selected_target)
@@ -110,7 +114,7 @@ class QGraph():
     def act(self,recent_history,soft=True):
         '''returns action by softmax'''
         q_probs = self.session.run(self.q_probs,{self.obs_t_ph:np.expand_dims(recent_history,axis=0)})[0]
-        
+
         try:
             qp = np.random.choice(q_probs,p=q_probs)
             action = np.argmax(q_probs == qp)

dqn/dqn_inverse2.py
@@ -147,7 +147,6 @@ def learn(env,
         # the prob of a trajectory = exponential of reward.
         #print(episode_storage)
 
-
         # Update reward samples (features)
         batch_size=25
         print('updating reward function')
@@ -167,10 +166,22 @@ def learn(env,
 
             # get mini batch of importance weights
             #w = np.array(episode_storage['episode_exp_rew'])[batch_idx_samp]/np.array(episode_storage['episode_prob_traj'][batch_idx_samp])
-            #w = np.array(episode_storage['episode_rewards'])[batch_idx_samp]-np.array(episode_storage['episode_prob_traj'])[batch_idx_samp] # its log prob actually
-            w = np.ones(batch_size)
+            w = np.array(episode_storage['episode_exp_rew'])[batch_idx_samp]-np.array(episode_storage['episode_prob_traj'])[batch_idx_samp] # its log prob actually
+            #w = np.ones(batch_size)
             loss = env.env.reward_func.update(batch_features_demo,
                 batch_features_samp,w)
+
+        feed_dict = {env.env.reward_func.batch_demo_features:batch_features_demo,
+            env.env.reward_func.batch_sample_features:batch_features_samp,
+            env.env.reward_func.w:w[:,np.newaxis],
+            }
+        w= session.run(env.env.reward_func.w,feed_dict=feed_dict)
+
+        print('w')
+        print(w)
+        print('loss')
+        print(loss)
+
         print('batch feature counts expert')
         print(batch_features_demo.mean(axis=0))
         print('batch features counts samples')
@@ -188,6 +199,17 @@ def learn(env,
             #learning_rate = optimizer_spec.lr_schedule.value(t)
             learning_rate = 0.0002
             q_graph.update(obs_t_batch, act_t_batch, rew_batch, obs_tp1_batch, done_mask,learning_rate)
+
+
+        feed_dict = {q_graph.obs_t_ph:obs_t_batch,
+                q_graph.act_t_ph:act_t_batch,
+                q_graph.rew_t_ph:rew_batch,
+                q_graph.obs_tp1_ph:obs_tp1_batch,
+                q_graph.done_mask_ph:done_mask,
+                q_graph.learning_rate:learning_rate}
+        print('example q vals')
+        print(session.run(q_graph.q_val_t,feed_dict=feed_dict))
+        print(session.run(q_graph.q_probs,feed_dict=feed_dict))
 
         # Update Target Network
         print('updating target network')

dqn/run_dqn_inverse.py
@@ -24,8 +24,10 @@ def main():
 
     # set up env (make sure this is the same as ground truth)
     env = cq.Wave1Env(num_aliens=2,num_crystals=20,num_asteroids=20,
-                  obs_type=3,relative_window=(25,25),
-                 reward_func=reward_func)
+                      obs_type=3,relative_window=(25,25),
+                     reward_func=reward_func,features=['crystal_captured',
+                        'asteroid_collision',
+                        'alien_collision'],stochastic_actions=False,choice_noise=.10)
 
     # random seed
     seed = 0 # Use a seed of zero (you may want to randomize the seed!)
@@ -33,12 +35,12 @@ def main():
     env.seed(seed)
 
     # saving
-    expt_dir ='cq_irl_11_29/'
+    expt_dir ='cq_irl_12_3_test/'
     #expt_dir ='test2'
     env = wrappers.Monitor(env, osp.join(expt_dir, "gym"), force=True)
 
     # load features for each trajectory and truncate to just the last 200 episode (which were the algorithm at optimal perf)
-    './cq'
+    #'./cq'
     crystals = np.loadtxt('cq_gr_truth_more_choice_noise4/gym/episode_crystals_captured2017-11-27-20:35.txt')[-500:-1]
     aliens = np.loadtxt('cq_gr_truth_more_choice_noise4/gym/episode_alien_collisions2017-11-27-20:35.txt')[-500:-1]
     asteroids = np.loadtxt('cq_gr_truth_more_choice_noise4/gym/episode_asteroid_collisions2017-11-27-20:35.txt')[-500:-1]